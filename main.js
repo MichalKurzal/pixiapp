cda3290b69a5ef5a6bbff20550cf9541dbe56777
Michal Kurzal <michalkurzal200@gmail.com>
2020-06-24 05:26:30 +0200

    .

--- main.js
+++ main.js
@@ -19,7 +19,29 @@ app.stage.interactive = true;
 console.log(app.stage.children);
 let site = '';
 
-const style = new PIXI.TextStyle({
+var watercolor =  new PIXI.Texture.fromImage("resources/watercolor.jpg");  //Loading texture Images 
+var watercolor2 =  new PIXI.Texture.fromImage("resources/watercolor2.jpg");
+var icon1tex =  new PIXI.Texture.fromImage("resources/healthcare-and-medical.png");
+var icon2tex = new PIXI.Texture.fromImage('resources/house-design.png');
+var icon3tex =  new PIXI.Texture.fromImage("resources/patent.png");
+var back2tex = new PIXI.Texture.fromImage("resources/watercolor3.jpg");
+var GTX1 = new PIXI.Texture.fromImage("resources/wind.jpg");
+var GR = new PIXI.Texture.fromImage("resources/greece.jpg");
+var iarrowtex = new PIXI.Texture.fromImage('resources/down-arrow.png');
+
+var back = new PIXI.Sprite(watercolor);  //Creating Sprites
+var back2 = new PIXI.Sprite(back2tex);
+var back3 = new PIXI.Sprite(watercolor2);
+var icon1 = new PIXI.Sprite(icon1tex);
+var icon2 = new PIXI.Sprite(icon2tex);
+var icon3 = new PIXI.Sprite(icon3tex);
+var img1 = new PIXI.Sprite(GTX1);
+var img2 = new PIXI.Sprite(GR);
+var iarrow = new PIXI.Sprite(iarrowtex);
+var iarrow2 = new PIXI.Sprite(iarrowtex);
+var iarrow3 = new PIXI.Sprite(iarrowtex);
+
+const style = new PIXI.TextStyle({  //Different Text Styles
     fontFamily: 'Tahoma',
     fontSize: 32,
     fontStyle: 'normal',
@@ -102,6 +124,60 @@ const style5 = new PIXI.TextStyle({
     wordWrapWidth: 320,
 });
 
+const richText = new PIXI.Text('Michal Kurzal Portfolio', style);
+gsap.to(richText,{x : window.innerWidth /2 -200, duration:2});
+richText.y =window.innerHeight/2;
+
+ let textlong = 'Hi, my name is Michal and I am a Javascript developer. ' 
++ 'I have solid skills in Javascript, Typscript and many Javascript frameworks like Angular. '+ 
+'I have also expiriences in mobile Ionic Apps Development. '+ 
+'My Hobbies are OpenGL and WebGL ';
+
+ let textlong2 =  'In my twenties I have been studying Sociolgy and Political Science at the University but I did not finish and I do not have a higher degree. ' 
++ 'In the recent past  I have been doing a professional apprentceship in the IT filed with focus on software development'
+
+let textlong3 =  'Michal Kurzal \n'+ 
+ 'Schneider Straße 4 \n'+
+ '65931 Frankfurt am Main\n \n'+
+ 'Deutschland/Germany\n \n'+
+
+'Images : My own Photos and pixabay.com \n'+
+'Icons : flaticon.com \n\n' +
+'JavaScript frameworks - Pixi.js and GSAP Green Sock \n\n'+
+ 'Privacy Policy \n \nPersonal data (usually referred to just as "data" below) will only be processed by me to the extent necessary and for the purpose of providing a functional and user-friendly website, including its contents, and the services offered there.'+
+'Per Art. 4 No. 1 of Regulation (EU) 2016/679, i.e. the General Data Protection Regulation (hereinafter referred to as the "GDPR"), "processing" refers to any operation or set of operations such as collection, recording, organization, structuring, storage, adaptation, alteration, retrieval, consultation, use, disclosure by transmission, dissemination, or otherwise making available, alignment, or combination, restriction, erasure, or destruction performed on personal data, whether by automated means or not.'+
+'The following privacy policy is intended to inform you in particular about the type, scope, purpose, duration, and legal basis for the processing of such data either under our own control or in conjunction with others. We also inform you below about the third-party components we use to optimize our website and improve the user experience which may result in said third parties also processing data they collect and control.\n \n'+
+
+'I. The rights of users and data subjects'+
+'With regard to the data processing to be described in more detail below, users and data subjects have the right'+
+
+'to confirmation of whether data concerning them is being processed, information about the data being processed, further information about the nature of the data processing, and copies of the data (cf. also Art. 15 GDPR)'+
+'to correct or complete incorrect or incomplete data (cf. also Art. 16 GDPR)'+
+'to the immediate deletion of data concerning them (cf. also Art. 17 DSGVO), or, alternatively, if further processing is necessary as stipulated in Art. 17 Para. 3 GDPR, to restrict said processing per Art. 18 GDPR'+
+'to receive copies of the data concerning them and/or provided by them and to have the same transmitted to other providers/controllers (cf. also Art. 20 GDPR)'+
+'to file complaints with the supervisory authority if they believe that data concerning them is being processed by the controller in breach of data protection provisions (see also Art. 77 GDPR).'+
+'In addition, the controller is obliged to inform all recipients to whom it discloses data of any such corrections, deletions, or restrictions placed on processing the same per Art. 16, 17 Para. 1, 18 GDPR. However, this obligation does not apply if such notification is impossible or involves a disproportionate effort. Nevertheless, users have a right to information about these recipients.'+
+
+'Likewise, under Art. 21 GDPR, users and data subjects have the right to object to the controllers future processing of their data pursuant to Art. 6 Para. 1 lit. f) GDPR. In particular, an objection to data processing for the purpose of direct advertising is permissible.\n \n'+
+
+'II. Information about the data processing \n'+
+'I am a private person and I do not colect any data. Only host of this site - github - colect some data. Please inform yourself abut github privacy policy'
+const icontext1 = new PIXI.Text('My Knowledge', style2);
+const icontext2 = new PIXI.Text('About', style2);
+const icontext3 = new PIXI.Text('Education', style2);
+
+icontext1.x = 55;
+icontext1.y = window.innerHeight;
+
+icontext2.x = window.innerWidth - 250;
+icontext2.y = window.innerHeight;
+
+icontext3.x = window.innerWidth/2 -100;
+icontext3.y = window.innerHeight;
+
+var page1text = new PIXI.Text(textlong, style3);
+var page1text2 = new PIXI.Text(textlong2, style3);
+var page1text3 = new PIXI.Text(textlong3, style4);
 
 let click = async ()=>{
     
@@ -309,83 +385,6 @@ let ihoverout3 =()=>{
     blurFilter1.blur = 0;
 }
 
-const richText = new PIXI.Text('Michal Kurzal Portfolio', style);
-gsap.to(richText,{x : window.innerWidth /2 -200, duration:2});
-richText.y =window.innerHeight/2;
-
- let textlong = 'Hi, my name is Michal and I am a Javascript developer. ' 
-+ 'I have solid skills in Javascript, Typscript and many Javascript frameworks like Angular. '+ 
-'I have also expiriences in mobile Ionic Apps Development. '+ 
-'My Hobbies are OpenGL and WebGL ';
-
- let textlong2 =  'In my twenties I have been studying Sociolgy and Political Science at the University but I did not finish and I do not have a higher degree. ' 
-+ 'In the recent past  I have been doing a professional apprentceship in the IT filed with focus on software development'
-
-let textlong3 =  'Michal Kurzal \n'+ 
- 'Schneider Straße 4 \n'+
- '65931 Frankfurt am Main\n \n'+
- 'Deutschland/Germany\n \n'+
-
-'Images : My own Photos and pixabay.com \n'+
-'Icons : flaticon.com \n\n' +
-'JavaScript frameworks - Pixi.js and GSAP Green Sock \n\n'+
- 'Privacy Policy \n \nPersonal data (usually referred to just as "data" below) will only be processed by me to the extent necessary and for the purpose of providing a functional and user-friendly website, including its contents, and the services offered there.'+
-'Per Art. 4 No. 1 of Regulation (EU) 2016/679, i.e. the General Data Protection Regulation (hereinafter referred to as the "GDPR"), "processing" refers to any operation or set of operations such as collection, recording, organization, structuring, storage, adaptation, alteration, retrieval, consultation, use, disclosure by transmission, dissemination, or otherwise making available, alignment, or combination, restriction, erasure, or destruction performed on personal data, whether by automated means or not.'+
-'The following privacy policy is intended to inform you in particular about the type, scope, purpose, duration, and legal basis for the processing of such data either under our own control or in conjunction with others. We also inform you below about the third-party components we use to optimize our website and improve the user experience which may result in said third parties also processing data they collect and control.\n \n'+
-
-'I. The rights of users and data subjects'+
-'With regard to the data processing to be described in more detail below, users and data subjects have the right'+
-
-'to confirmation of whether data concerning them is being processed, information about the data being processed, further information about the nature of the data processing, and copies of the data (cf. also Art. 15 GDPR)'+
-'to correct or complete incorrect or incomplete data (cf. also Art. 16 GDPR)'+
-'to the immediate deletion of data concerning them (cf. also Art. 17 DSGVO), or, alternatively, if further processing is necessary as stipulated in Art. 17 Para. 3 GDPR, to restrict said processing per Art. 18 GDPR'+
-'to receive copies of the data concerning them and/or provided by them and to have the same transmitted to other providers/controllers (cf. also Art. 20 GDPR)'+
-'to file complaints with the supervisory authority if they believe that data concerning them is being processed by the controller in breach of data protection provisions (see also Art. 77 GDPR).'+
-'In addition, the controller is obliged to inform all recipients to whom it discloses data of any such corrections, deletions, or restrictions placed on processing the same per Art. 16, 17 Para. 1, 18 GDPR. However, this obligation does not apply if such notification is impossible or involves a disproportionate effort. Nevertheless, users have a right to information about these recipients.'+
-
-'Likewise, under Art. 21 GDPR, users and data subjects have the right to object to the controllers future processing of their data pursuant to Art. 6 Para. 1 lit. f) GDPR. In particular, an objection to data processing for the purpose of direct advertising is permissible.\n \n'+
-
-'II. Information about the data processing \n'+
-'I am a private person and I do not colect any data. Only host of this site - github - colect some data. Please inform yourself abut github privacy policy'
-const icontext1 = new PIXI.Text('My Knowledge', style2);
-const icontext2 = new PIXI.Text('About', style2);
-const icontext3 = new PIXI.Text('Education', style2);
-
-icontext1.x = 55;
-icontext1.y = window.innerHeight;
-
-icontext2.x = window.innerWidth - 250;
-icontext2.y = window.innerHeight;
-
-icontext3.x = window.innerWidth/2 -100;
-icontext3.y = window.innerHeight;
-
-var page1text = new PIXI.Text(textlong, style3);
-var page1text2 = new PIXI.Text(textlong2, style3);
-var page1text3 = new PIXI.Text(textlong3, style4);
-
-var watercolor =  new PIXI.Texture.fromImage("resources/watercolor.jpg");
-var watercolor2 =  new PIXI.Texture.fromImage("resources/watercolor2.jpg");
-var icon1tex =  new PIXI.Texture.fromImage("resources/healthcare-and-medical.png");
-var icon2tex = new PIXI.Texture.fromImage('resources/house-design.png');
-var icon3tex =  new PIXI.Texture.fromImage("resources/patent.png");
-var back2tex = new PIXI.Texture.fromImage("resources/watercolor3.jpg");
-var GTX1 = new PIXI.Texture.fromImage("resources/wind.jpg");
-var GR = new PIXI.Texture.fromImage("resources/greece.jpg");
-var iarrowtex = new PIXI.Texture.fromImage('resources/down-arrow.png');
-
-var back = new PIXI.Sprite(watercolor);
-var back2 = new PIXI.Sprite(back2tex);
-var back3 = new PIXI.Sprite(watercolor2);
-var icon1 = new PIXI.Sprite(icon1tex);
-var icon2 = new PIXI.Sprite(icon2tex);
-var icon3 = new PIXI.Sprite(icon3tex);
-var img1 = new PIXI.Sprite(GTX1);
-var img2 = new PIXI.Sprite(GR);
-var iarrow = new PIXI.Sprite(iarrowtex);
-var iarrow2 = new PIXI.Sprite(iarrowtex);
-var iarrow3 = new PIXI.Sprite(iarrowtex);
-
 iarrow.angle = 90;
 iarrow3.angle = 180;
 
@@ -463,16 +462,10 @@ img1.interactive = true;
 
 cont1.filters = [ displacementFilter];
 
-
-
 cont2.addChild(richText);
 cont2.addChild(icontext1);
 cont2.addChild(icontext2);
 cont2.addChild(icontext3);
-
-
-
-
 
 let is = async() =>{
     
@@ -816,8 +809,4 @@ app.ticker.add((delta) => {
     displacementSprite.position.y += 0.3 * delta;
     app.renderer.render(cont1);
     app.renderer.render(cont2);
-
-    //gsap.to(richText, { x: window.innerWidth /2 -200, duration: 2});
-    
-   // gsap.to(icon2, {x : window.innerWidth -300, duration:2})
 });
